7b0f2710590692fda9ae982fbcf9d5fa33147703
luischavez23 <lchavezdiazdesandi@gmail.com>
2026-05-20 13:15:50 -0600

    Validate form

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -67,106 +67,111 @@ const RSVP = () => {
       data-aos="fade-up"
       data-aos-delay="500"
     >
-      <div className="max-w-xl mx-auto bg-dark-soft  py-12 px-8 md:p-20 shadow-2xl border-t-4 border-gold">
+      <div className="max-w-xl mx-auto bg-dark-soft py-12 px-8 md:p-20 shadow-2xl border-t-4 border-gold">
 
         {!success ? (
           <>
-          <h2 className="text-4xl text-center mb-4 italic text-gold">
-            Confirmar Asistencia
-          </h2>
+            <h2 className="text-4xl text-center mb-4 italic text-gold">
+              Confirmar Asistencia
+            </h2>
 
-          <p className="text-center mb-12 text-[10px] uppercase tracking-[0.4em] text-white/60">
-            Agradecemos tu respuesta antes del 14 de Noviembre
-          </p>
-          <form
-            onSubmit={handleSubmit}
-            className="space-y-10"
-          >
-            {/* Nombre */}
-            <div className="relative">
-              <input
-                type="text"
-                name="name"
-                placeholder="Nombre completo"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full bg-transparent py-3 focus:outline-none placeholder:text-white/20 font-light italic border-b border-white/10 text-white"
-                required
-              />
-            </div>
+            <p className="text-center mb-12 text-[10px] uppercase tracking-[0.4em] text-white/60">
+              Agradecemos tu respuesta antes del 14 de Noviembre
+            </p>
 
-            {/* Asistencia */}
-            <div className="space-y-4">
-              <label className="block text-[10px] uppercase tracking-[0.2em] font-bold text-gold">
-                ¿Nos acompañas?
-              </label>
+            <form onSubmit={handleSubmit} className="space-y-10">
 
-              <div className="flex gap-10 flex-col md:flex-row">
-                <label className="flex items-center cursor-pointer text-sm font-light italic">
-                  <input
-                    type="radio"
-                    name="attend"
-                    value="si"
-                    checked={formData.attend === "si"}
-                    onChange={handleChange}
-                    className="mr-3 accent-gold"
-                    required
-                  />
-                  Sí, acepto
-                </label>
-
-                <label className="flex items-center cursor-pointer text-sm font-light italic">
-                  <input
-                    type="radio"
-                    name="attend"
-                    value="no"
-                    checked={formData.attend === "no"}
-                    onChange={handleChange}
-                    className="mr-3 accent-gold"
-                  />
-                  No podré ir
-                </label>
+              {/* Nombre */}
+              <div className="relative">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nombre completo"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full bg-transparent py-3 focus:outline-none placeholder:text-white/20 font-light italic border-b border-white/10 text-white"
+                  required
+                  onInvalid={(e) =>
+                    e.target.setCustomValidity(
+                      "Please type your name."
+                    )
+                  }
+                  onInput={(e) => e.target.setCustomValidity("")}
+                />
               </div>
-            </div>
+              
+              {/* Asistencia */}
+              <div className="space-y-4">
+                <label className="block text-[10px] uppercase tracking-[0.2em] font-bold text-gold">
+                  ¿Nos acompañas?
+                </label>
 
-            {/* Mensaje */}
-            <div className="space-y-4">
-              <label className="block text-[10px] uppercase tracking-[0.2em] font-bold text-gold">
-                Déjanos un mensaje
-              </label>
+                <div className="flex gap-10 flex-col md:flex-row">
+                  <label className="flex items-center cursor-pointer text-sm font-light italic">
+                    <input
+                      type="radio"
+                      name="attend"
+                      value="si"
+                      checked={formData.attend === "si"}
+                      onChange={handleChange}
+                      className="mr-3 accent-gold"
+                      required
+                    />
+                    Sí, acepto
+                  </label>
 
-              <textarea
-                name="message"
-                rows="4"
-                value={formData.message}
-                onChange={handleChange}
-                className="w-full bg-transparent py-3 px-3
-                focus:outline-none
-                placeholder:text-white/20
-                font-light italic
-                border border-white/10
-                text-white
-                resize-none"
-              />
-            </div>
+                  <label className="flex items-center cursor-pointer text-sm font-light italic">
+                    <input
+                      type="radio"
+                      name="attend"
+                      value="no"
+                      checked={formData.attend === "no"}
+                      onChange={handleChange}
+                      className="mr-3 accent-gold"
+                    />
+                    No podré ir
+                  </label>
+                </div>
+              </div>
 
-            {/* Botón */}
-            <button
-              type="submit"
-              disabled={loading}
-              className="w-full btn-custom py-5 tracking-[0.5em] text-[8px] md:text-[10px] font-bold uppercase shadow-lg"
-            >
-              {loading ? "Enviando..." : "Enviar mi Confirmación"}
-            </button>
+              {/* Mensaje */}
+              <div className="space-y-4">
+                <label className="block text-[10px] uppercase tracking-[0.2em] font-bold text-gold">
+                  Déjanos un mensaje
+                </label>
 
-            {/* Error */}
-            {error && (
-              <p className="text-red-400 text-center">
-                {error}
-              </p>
-            )}
-          </form>
-        </>
+                <textarea
+                  name="message"
+                  rows="4"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="w-full bg-transparent py-3 px-3
+                  focus:outline-none
+                  placeholder:text-white/20
+                  font-light italic
+                  border border-white/10
+                  text-white
+                  resize-none"
+                />
+              </div>
+
+              {/* Botón */}
+              <button
+                type="submit"
+                disabled={loading}
+                className="w-full btn-custom py-5 tracking-[0.5em] text-[8px] md:text-[10px] font-bold uppercase shadow-lg"
+              >
+                {loading ? "Enviando..." : "Enviar mi Confirmación"}
+              </button>
+
+              {/* Error */}
+              {error && (
+                <p className="text-red-400 text-center">
+                  {error}
+                </p>
+              )}
+            </form>
+          </>
         ) : (
           <div className="text-center py-10">
             <h3 className="text-3xl md:text-4xl italic serif text-gold mb-6">
