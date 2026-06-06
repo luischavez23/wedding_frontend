bffd585bffbad98b532191a8a3e172db7e6758c3
luischavez23 <lchavezdiazdesandi@gmail.com>
2026-06-06 14:52:55 -0600

    Add new pictures

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="max-w-5xl mx-auto grid md:grid-cols-2 gap-20">
             <div className="text-center">
                 <div className="relative inline-block mb-8">
-                    <img src={eduardoImg} alt="Eduardo" className="w-48 h-48 object-cover brightness-75 opacity-80 rounded-full shadow-lg border-2 border-gold p-1" />
+                    <img src={eduardoImg} alt="Eduardo" className="w-48 h-48 object-cover  rounded-full shadow-lg border-2 border-gold p-1" />
                 </div>
                 <p className="serif text-xl italic leading-relaxed opacity-90">"Ella es mi refugio donde paso todas las tormentas y es el lugar donde encuentro mi calma y la paz en mi alma."</p>
                 <div className="mt-4 h-px w-12 bg-gold mx-auto mb-2"></div>
@@ -16,7 +16,7 @@ const About = () => {
             </div>
             <div className="text-center">
                 <div className="relative inline-block mb-8">
-                    <img src={lupitaImg} alt="Guadalupe" className="w-48 h-48 object-cover brightness-75 opacity-80 rounded-full shadow-lg border-2 border-gold p-1" />
+                    <img src={lupitaImg} alt="Guadalupe" className="w-48 h-48 object-cover  rounded-full shadow-lg border-2 border-gold p-1" />
                 </div>
                 <p className="serif text-xl italic leading-relaxed opacity-90">"Eres mi hogar, mi vida y mi amor, contigo nada es fácil pero cada experiencia siempre es lo mejor; en los retos y desafíos has sido mi compañero y mi fuerza...no hay nadie más con quién quiera compartir mi vida."</p>
                 <div className="mt-4 h-px w-12 bg-gold mx-auto mb-2"></div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 
 function Footer() {
   return (
-   <footer class="py-20 text-center bg-black border-t border-white/5">
-        <div class="mb-8">
-            <span class="text-2xl italic serif text-gold">Guadalupe & Eduardo</span>
+   <footer className="py-20 text-center bg-black border-t border-white/5">
+        <div className="mb-8">
+            <span className="text-2xl italic serif text-gold">Guadalupe & Eduardo</span>
         </div>
-        <p class="text-[9px] uppercase tracking-[0.5em] opacity-30">Hecho por Luis Pablo Chavez</p>
+        <p className="text-[9px] uppercase tracking-[0.5em] opacity-30">Hecho por Luis Pablo Chavez</p>
     </footer>
   )
 }
